5a064b6ed888cf735aba7c0265b572c38798011b
Manextra <manextra123321@gmail.com>
2025-11-20 20:33:46 +0530

    YourDost

--- DSA/SecondLargestUnique.cpp
+++ DSA/SecondLargestUnique.cpp
@@ -30,9 +30,15 @@ int secondLargestUnique(vector<int>& nums) {
 int main() {
     vector<int> test1 = {3, 5, 2, 5, 6, 6, 1};
     vector<int> test2 = {7, 7, 7};
+    vector<int> test3 = {1, 2, 3, 4, 5};
+    vector<int> test4 = {5, 5, 4, 4, 3, 3};
+    vector<int> test5 = {10};
     
     cout << "Input: [3, 5, 2, 5, 6, 6, 1] -> Output: " << secondLargestUnique(test1) << endl;
     cout << "Input: [7, 7, 7] -> Output: " << secondLargestUnique(test2) << endl;
+    cout << "Input: [1, 2, 3, 4, 5] -> Output: " << secondLargestUnique(test3) << endl;
+    cout << "Input: [5, 5, 4, 4, 3, 3] -> Output: " << secondLargestUnique(test4) << endl;
+    cout << "Input: [10] -> Output: " << secondLargestUnique(test5) << endl;
     
     return 0;
 }
